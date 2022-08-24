ed39050c4a1236698142b14fad31b122dd2d700f
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-24 09:55:28 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form');
+ form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  handleTodo(e.target.new_task.value);
+  form.reset();
+ })
+  // ynction handleTodo(todo)
